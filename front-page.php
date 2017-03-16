d81f809ddf173470544d1ec17967e4dba8a4afac
wwbewbe <what.wbewbe@gmail.com>
2017-03-16 09:30:32 +0900

    first commit

---
+++ front-page.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * The template for displaying front-page
+ */
+get_header(); ?>
+	<div class="row front-feature">
+	<?php if ( get_page_by_path( 'concept' ) ) : ?>
+		<div class="large-4 columns">
+			<a href="<?php echo get_permalink( get_page_by_path( 'concept' )->ID ); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/1.jpg" alt=""></a>
+			<h5><a href="<?php echo get_permalink( get_page_by_path( 'concept' )->ID ); ?>"><?php echo esc_html__( 'Front Menu 1', 'bp_origin' ); ?></a></h5>
+			<p><?php echo esc_html__( 'Front Menu description sample 1. This menu sample to show description and link to page xxx/concept', 'bp_origin' ); ?></p>
+		</div>
+	<?php endif; ?>
+	<?php if ( get_page_by_path( 'menu' ) ) : ?>
+		<div class="large-4 columns">
+			<a href="<?php echo get_permalink( get_page_by_path( 'menu' )->ID ); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/2.jpg" alt=""></a>
+			<h5><a href="<?php echo get_permalink( get_page_by_path( 'menu' )->ID ); ?>"><?php echo esc_html__( 'Front Menu 2', 'bp_origin' ); ?></a></h5>
+			<p><?php echo esc_html__( 'Front Menu description sample 2. This menu sample to show description and link to page xxx/menu', 'bp_origin' ); ?></p>
+		</div>
+	<?php endif; ?>
+	<?php if ( get_page_by_path( 'access' ) ) : ?>
+		<div class="large-4 columns">
+			<a href="<?php echo get_permalink( get_page_by_path( 'access' )->ID ); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/3.jpg" alt=""></a>
+			<h5><a href="<?php echo get_permalink( get_page_by_path( 'access' )->ID ); ?>"><?php echo esc_html__( 'Front Menu 3', 'bp_origin' ); ?></a></h5>
+			<p><?php echo esc_html__( 'Front Menu description sample 3. This menu sample to show description and link to page xxx/access', 'bp_origin' ); ?></p>
+		</div>
+	<?php endif; ?>
+	</div>
+
+	<div class="front-news">
+		<div class="row">
+			<div class="large-12 columns">
+				<h3><?php echo esc_html__( 'Latest Information', 'bp_origin' ); ?></h3>
+				<div class="row">
+					<?php
+					$args = array(
+						'posts_per_page'=> '4',
+					);
+					$the_query = new WP_Query($args); ?>
+					<?php if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) :
+						$the_query->the_post(); ?>
+					<div class="large-3 small-12 columns">
+						<a href="<?php the_permalink(); ?>">
+							<div class="newspost">
+								<div class="row">
+									<div class="large-12 small-3 columns">
+										<div class="thumbnail">
+											<img src="<?php echo get_thumbnail_url( 'top-tumb' ); ?>" alt="" title="" />
+										</div>
+									</div>
+
+									<div class="large-12 small-9 columns">
+										<div class="news-meta">
+											<div class="date">
+												<i class="fa fa-pencil fa-fw" aria-hidden="true"></i>
+												<?php the_time( 'Y/m/d' ); ?>
+											</div>
+											<p>
+												<?php echo mb_substr( get_the_title(), 0, 40 ); ?>
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</a>
+					</div>
+				<?php endwhile; endif; ?>
+				<?php wp_reset_query(); ?>
+				</div>
+			</div>
+		</div> <!-- /row -->
+	</div> <!-- /front-news -->
+
+	<div class="front-sp">
+		<div class="row">
+			<div class="large-6 columns">
+				<div class="circle">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/4.jpg" alt="" />
+				</div>
+			</div> <!--  -->
+
+			<div class="large-6 columns">
+				<h2>みんなの来店を<br />待ってるワンッ！</h2>
+				<p>たまに出勤する、店長のマルくんです。</p>
+			</div>
+		</div>
+	</div>
+	<div id="main" class="site-main row">
+<?php get_footer(); ?>
---
+++ template-parts/content.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package BP-Origin
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(''); ?>>
+	<div class="row">
+		<div class="large-3 small-3 columns thumbnail">
+			<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( esc_html__( 'Permalink to %s', 'bp_origin' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark">
+					<img src="<?php echo get_thumbnail_url( 'mideum' ); ?>" alt="" title="" />
+			</a>
+		</div> <!-- 3 thumbnail -->
+		<div class="large-9 small-9 columns">
+			<header class="entry-header">
+				<h3 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( esc_html__( 'Permalink to %s', 'bp_origin' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
+
+				<?php if ( 'post' == get_post_type() ) : ?>
+				<div class="entry-meta">
+					<?php bp_origin_posted_on(); ?>
+				</div><!-- .entry-meta -->
+				<?php endif; ?>
+			</header><!-- .entry-header -->
+
+			<div class="entry-summary">
+				<?php the_excerpt(); ?>
+			</div><!-- .entry-summary -->
+
+			<footer class="entry-meta">
+			<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
+				<?php
+					/* translators: used between list items, there is a space after the comma */
+					$categories_list = get_the_category_list( esc_html__( ', ', 'bp_origin' ) );
+					if ( $categories_list && bp_origin_categorized_blog() ) :
+				?>
+				<span class="cat-links">
+					<?php printf( '<i class="fa fa-tags fa-fw" aria-hidden="true"></i>%1$s', $categories_list ); ?>
+				</span>
+				<?php endif; // End if categories ?>
+
+				<?php
+					/* translators: used between list items, there is a space after the comma */
+					$tags_list = get_the_tag_list( '', esc_html__( ', ', 'bp_origin' ) );
+					if ( $tags_list ) :
+				?>
+				<span class="tags-links">
+					<?php printf( '%1$s', $tags_list ); ?>
+				</span>
+				<?php endif; // End if $tags_list ?>
+			<?php endif; // End if 'post' == get_post_type() ?>
+
+			<?php edit_post_link( esc_html__( 'Edit', 'bp_origin' ), '<span class="edit-link">', '</span>' ); ?>
+			</footer><!-- .entry-meta -->
+		</div><!-- 9 title excerpt -->
+	</div><!-- .row -->
+</article><!-- #post-## -->
