83d41adc579b9354ae0460524ff35f5ff0a075f0
wwbewbe <what.wbewbe@gmail.com>
2017-04-27 17:18:10 +0900

    Tab to Space by ATOM

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -4,7 +4,7 @@
  *
  * You can add an optional custom header image to header.php like so ...
  *
-	<?php the_header_image_tag(); ?>
+    <?php the_header_image_tag(); ?>
  *
  * @link https://developer.wordpress.org/themes/functionality/custom-headers/
  *
@@ -17,14 +17,14 @@
  * @uses bp_origin_header_style()
  */
 function bp_origin_custom_header_setup() {
-	add_theme_support( 'custom-header', apply_filters( 'bp_origin_custom_header_args', array(
-		'default-image'          => '%s/img/main.jpg',
-		'default-text-color'     => '000000',
-		'width'                  => 1600,
-		'height'                 => 400,
-		'flex-height'            => true,
-		'wp-head-callback'       => 'bp_origin_header_style',
-	) ) );
+    add_theme_support( 'custom-header', apply_filters( 'bp_origin_custom_header_args', array(
+        'default-image'          => '%s/img/main.jpg',
+        'default-text-color'     => '000000',
+        'width'                  => 1600,
+        'height'                 => 400,
+        'flex-height'            => true,
+        'wp-head-callback'       => 'bp_origin_header_style',
+    ) ) );
 }
 add_action( 'after_setup_theme', 'bp_origin_custom_header_setup' );
 
@@ -35,47 +35,47 @@ if ( ! function_exists( 'bp_origin_header_style' ) ) :
  * @see bp_origin_custom_header_setup().
  */
 function bp_origin_header_style() {
-	$header_text_color = get_header_textcolor();
+    $header_text_color = get_header_textcolor();
 
-	/*
-	 * If no custom options for text are set, let's bail.
-	 * get_header_textcolor() options: Any hex value, 'blank' to hide text. Default: add_theme_support( 'custom-header' ).
-	 */
-	if ( get_theme_support( 'custom-header', 'default-text-color' ) === $header_text_color ) {
-		return;
-	}
+    /*
+     * If no custom options for text are set, let's bail.
+     * get_header_textcolor() options: Any hex value, 'blank' to hide text. Default: add_theme_support( 'custom-header' ).
+     */
+    if ( get_theme_support( 'custom-header', 'default-text-color' ) === $header_text_color ) {
+        return;
+    }
 
-	// If we get this far, we have custom styles. Let's do this.
-	?>
-	<style type="text/css">
-	<?php
-		// Has the text been hidden?
-		if ( ! display_header_text() ) :
-	?>
-		.site-title,
-		.site-description {
-			position: absolute;
-			clip: rect(1px, 1px, 1px, 1px);
-		}
-	<?php
-		// If the user has set a custom color for the text use that.
-		else :
-	?>
-		#main-img{
-			position: relative;
-		}
-		.site-title a,
-		.site-description {
-			color: #<?php echo esc_attr( $header_text_color ); ?>;
-			position: absolute;
-			top: 50%;
-			left: 50%;
-			text-align: center;
-			transition: translate(-50%,-50%);
-			text-shadow: 0 0 10px #000000;
-		}
-	<?php endif; ?>
-	</style>
-	<?php
+    // If we get this far, we have custom styles. Let's do this.
+    ?>
+    <style type="text/css">
+    <?php
+        // Has the text been hidden?
+        if ( ! display_header_text() ) :
+    ?>
+        .site-title,
+        .site-description {
+            position: absolute;
+            clip: rect(1px, 1px, 1px, 1px);
+        }
+    <?php
+        // If the user has set a custom color for the text use that.
+        else :
+    ?>
+        #main-img{
+            position: relative;
+        }
+        .site-title a,
+        .site-description {
+            color: #<?php echo esc_attr( $header_text_color ); ?>;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            text-align: center;
+            transition: translate(-50%,-50%);
+            text-shadow: 0 0 10px #000000;
+        }
+    <?php endif; ?>
+    </style>
+    <?php
 }
 endif;
--- js/header-fix.js
+++ js/header-fix.js
@@ -1,13 +1,13 @@
 (function($) {
-	$(function($) {
-		var navfit = $('.main-navi');
-		var navtop = navfit.offset().top;
-		$(window).scroll(function () {
-		if($(window).scrollTop() > navtop) {
-			navfit.addClass('fixed');
-		} else {
-			navfit.removeClass('fixed');
-		}
-		});
-	});
+    $(function($) {
+        var navfit = $('.main-navi');
+        var navtop = navfit.offset().top;
+        $(window).scroll(function () {
+        if($(window).scrollTop() > navtop) {
+            navfit.addClass('fixed');
+        } else {
+            navfit.removeClass('fixed');
+        }
+        });
+    });
 })(jQuery);
