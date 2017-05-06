fdc381b260bfcaa8abc7c7d2ee05001a2c62e5e8
wwbewbe <what.wbewbe@gmail.com>
2017-05-06 14:09:41 +0900

    change space to tab

--- 404.php
+++ 404.php
@@ -9,61 +9,61 @@
 
 get_header(); ?>
 
-    <div id="primary" class="content-area large-9 columns">
-        <main id="main" class="site-main" role="main">
+	<div id="primary" class="content-area large-9 columns">
+		<main id="main" class="site-main" role="main">
 
-            <section class="error-404 not-found">
-                <header class="page-header entry-header">
-                    <h1 class="page-title entry-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'bp_origin' ); ?></h1>
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/404.png" alt="みつかりませんでしたぷー。" />
-                </header><!-- .page-header -->
+			<section class="error-404 not-found">
+				<header class="page-header entry-header">
+					<h1 class="page-title entry-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'bp_origin' ); ?></h1>
+					<img src="<?php echo get_template_directory_uri(); ?>/img/404.png" alt="みつかりませんでしたぷー。" />
+				</header><!-- .page-header -->
 
-                <div class="page-content">
-                    <p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'bp_origin' ); ?></p>
+				<div class="page-content">
+					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'bp_origin' ); ?></p>
 
-                    <?php
-                        get_search_form();
+					<?php
+						get_search_form();
 
-                        the_widget( 'BP_Widget_Recent_Posts' );
+						the_widget( 'BP_Widget_Recent_Posts' );
 
-                        // Only show the widget if site has multiple categories.
-                        if ( bp_origin_categorized_blog() ) :
-                    ?>
+						// Only show the widget if site has multiple categories.
+						if ( bp_origin_categorized_blog() ) :
+					?>
 
-                    <div class="widget widget_categories">
-                        <h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'bp_origin' ); ?></h2>
-                        <ul>
-                        <?php
-                            wp_list_categories( array(
-                                'orderby'    => 'count',
-                                'order'      => 'DESC',
-                                'show_count' => 1,
-                                'title_li'   => '',
-                                'number'     => 10,
-                            ) );
-                        ?>
-                        </ul>
-                    </div><!-- .widget -->
+					<div class="widget widget_categories">
+						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'bp_origin' ); ?></h2>
+						<ul>
+						<?php
+							wp_list_categories( array(
+								'orderby'    => 'count',
+								'order'      => 'DESC',
+								'show_count' => 1,
+								'title_li'   => '',
+								'number'     => 10,
+							) );
+						?>
+						</ul>
+					</div><!-- .widget -->
 
-                    <?php
-                        endif;
+					<?php
+						endif;
 
-                        /* translators: %1$s: smiley */
-                        $archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'bp_origin' ), convert_smilies( ':)' ) ) . '</p>';
-                        the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
+						/* translators: %1$s: smiley */
+						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'bp_origin' ), convert_smilies( ':)' ) ) . '</p>';
+						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
 
-                        the_widget( 'WP_Widget_Tag_Cloud' );
-                    ?>
+						the_widget( 'WP_Widget_Tag_Cloud' );
+					?>
 
-                    <p><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-                        <?php bloginfo( 'name' ); ?>
-                        トップページへ戻る</a>
-                    </p>
-                </div><!-- .page-content -->
-            </section><!-- .error-404 -->
+					<p><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+						<?php bloginfo( 'name' ); ?>
+						トップページへ戻る</a>
+					</p>
+				</div><!-- .page-content -->
+			</section><!-- .error-404 -->
 
-        </main><!-- #main -->
-    </div><!-- #primary -->
+		</main><!-- #main -->
+	</div><!-- #primary -->
 <?php get_sidebar(); ?>
 <?php
 get_footer();
--- header.php
+++ header.php
@@ -15,34 +15,34 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <?php if( is_home() || is_front_page() ): // トップページ用のメタデータ ?>
-    <meta property="og:type" content="website">
-    <meta property="og:title" content="<?php bloginfo( 'name' ); ?>">
-    <meta property="og:url" content="<?php echo esc_url(home_url( '/' )); ?>">
-    <meta property="og:description" content="<?php echo esc_attr( wp_trim_words( $post->post_excerpt, 100, '…' ) ); ?>">
-    <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/img/site-top.png">
+	<meta property="og:type" content="website">
+	<meta property="og:title" content="<?php bloginfo( 'name' ); ?>">
+	<meta property="og:url" content="<?php echo esc_url(home_url( '/' )); ?>">
+	<meta property="og:description" content="<?php echo esc_attr( wp_trim_words( $post->post_excerpt, 100, '…' ) ); ?>">
+	<meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/img/site-top.png">
 <?php endif; // トップページ用のメタデータ【ここまで】 ?>
 <?php if( is_single() || is_page() ): //記事の個別ページ用のメタデータ ?>
-    <meta name="description" content="<?php echo wp_trim_words( $post->post_content, 100, '…' ); ?>">
-    <meta property="og:type" content="article">
-    <meta property="og:title" content="<?php the_title(); ?>">
-    <meta property="og:url" content="<?php the_permalink(); ?>">
-    <meta property="og:description" content="<?php echo esc_attr( wp_trim_words( $post->post_excerpt, 100, '…' ) ); ?>">
-    <meta property="og:image" content="<?php echo get_thumbnail_url( 'large' ); ?>">
+	<meta name="description" content="<?php echo wp_trim_words( $post->post_content, 100, '…' ); ?>">
+	<meta property="og:type" content="article">
+	<meta property="og:title" content="<?php the_title(); ?>">
+	<meta property="og:url" content="<?php the_permalink(); ?>">
+	<meta property="og:description" content="<?php echo esc_attr( wp_trim_words( $post->post_excerpt, 100, '…' ) ); ?>">
+	<meta property="og:image" content="<?php echo get_thumbnail_url( 'large' ); ?>">
 <?php endif; //記事の個別ページ用のメタデータ【ここまで】?>
 <?php if( is_category() || is_tag() ): // カテゴリー・タグページ用のメタデータ ?>
 <?php if( is_category() ) {
-        $termid = $cat;
-        $taxname = 'category';
+		$termid = $cat;
+		$taxname = 'category';
 } elseif( is_tag() ) {
-        $termid = $tag_id;
-        $taxname = 'post_tag';
+		$termid = $tag_id;
+		$taxname = 'post_tag';
 } ?>
-    <meta name="description" content="<?php single_term_title(); ?><?php echo esc_attr(__(' Events List', 'SagasWhat')); ?>">
-    <meta property="og:type" content="website">
-    <meta property="og:title" content="<?php single_term_title(); ?> | <?php bloginfo( 'name' ); ?>">
-    <meta property="og:url" content="<?php echo get_term_link( $termid, $taxname ); ?>">
-    <meta property="og:description" content="<?php single_term_title(); ?><?php echo esc_attr(__(' Events List', 'SagasWhat')); ?>">
-    <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/img/site-top.png">
+	<meta name="description" content="<?php single_term_title(); ?><?php echo esc_attr(__(' Events List', 'SagasWhat')); ?>">
+	<meta property="og:type" content="website">
+	<meta property="og:title" content="<?php single_term_title(); ?> | <?php bloginfo( 'name' ); ?>">
+	<meta property="og:url" content="<?php echo get_term_link( $termid, $taxname ); ?>">
+	<meta property="og:description" content="<?php single_term_title(); ?><?php echo esc_attr(__(' Events List', 'SagasWhat')); ?>">
+	<meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/img/site-top.png">
 <?php endif; // カテゴリ・タグページ用のメタデータ【ここまで】 ?>
 
 <meta property="og:site_name" content="<?php bloginfo( 'name' ); ?>">
@@ -55,21 +55,21 @@
 
 <div id="fb-root"></div>
 <?php if ( 'ja' == get_bloginfo('language') ) : ?>
-    <script>(function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.8";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));</script>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.8";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
 <?php else : ?>
-    <script>(function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));</script>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
 <?php endif; ?>
 
 <script src="https://apis.google.com/js/platform.js" async defer></script>
@@ -81,57 +81,57 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-    <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'bp_origin' ); ?></a>
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'bp_origin' ); ?></a>
 
-    <header id="masthead" class="site-header" role="banner">
-        <div class="row">
-            <div class="site-branding large-6 columns">
-                <h1 class="site-logo">
-                    <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-                        <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="wpd-cafe" />
-                    </a>
-                </h1>
-            </div>
-            <div class="site-address large-6 columns">
-                <i class="fa fa-angle-double-right" aria-hidden="true"></i>Any wording display here<br />you can write second Line
-            </div>
-        </div>
-        <div id="site-navi" class="main-navi">
-            <div id="small-navi"></div>
-            <div class="row">
-                <div class="large-12 columns">
-                    <button type="button" id="navbtn">
-                    <i class="fa fa-bars"></i><span>MENU</span>
-                    </button>
-                    <?php wp_nav_menu( array(
-                                    'theme_location' => 'primary',
-                                    'container' => 'nav',
-                                    'container_class' => 'header-nav',
-                                    'container_id' => 'header-nav'
-                    ) ); ?>
-                </div>
-            </div>
-        </div>
-        <!-- #site-navigation -->
-    </header>
-    <!-- #masthead -->
+	<header id="masthead" class="site-header" role="banner">
+		<div class="row">
+			<div class="site-branding large-6 columns">
+				<h1 class="site-logo">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="wpd-cafe" />
+					</a>
+				</h1>
+			</div>
+			<div class="site-address large-6 columns">
+				<i class="fa fa-angle-double-right" aria-hidden="true"></i>Any wording display here<br />you can write second Line
+			</div>
+		</div>
+		<div id="site-navi" class="main-navi">
+			<div id="small-navi"></div>
+			<div class="row">
+				<div class="large-12 columns">
+					<button type="button" id="navbtn">
+					<i class="fa fa-bars"></i><span>MENU</span>
+					</button>
+					<?php wp_nav_menu( array(
+									'theme_location' => 'primary',
+									'container' => 'nav',
+									'container_class' => 'header-nav',
+									'container_id' => 'header-nav'
+					) ); ?>
+				</div>
+			</div>
+		</div>
+		<!-- #site-navigation -->
+	</header>
+	<!-- #masthead -->
 <?php if ( is_home() || is_front_page() ) : ?>
-    <div id="main-img">
-        <img src="<?php header_image(); ?>" alt="" />
-        <h1 class="site-description">
-            <?php bloginfo( 'description' ); ?>
-        </h1>
-    </div>
+	<div id="main-img">
+		<img src="<?php header_image(); ?>" alt="" />
+		<h1 class="site-description">
+			<?php bloginfo( 'description' ); ?>
+		</h1>
+	</div>
 <?php endif; ?>
 
 <?php if ( ! is_home() && ! is_front_page() && is_bp_plugin_active( 'show-adsense/show-adsense.php' ) ) : ?>
-    <div class="row">
-        <div class="large-12 columns">
-            <?php echo do_shortcode( '[showad id="header"]' ); ?>
-        </div>
-    </div>
+	<div class="row">
+		<div class="large-12 columns">
+			<?php echo do_shortcode( '[showad id="header"]' ); ?>
+		</div>
+	</div>
 <?php endif; ?>
 
 <?php if ( ! is_front_page() ) : ?>
-    <div id="content" class="site-content row">
+	<div id="content" class="site-content row">
 <?php endif; ?>
